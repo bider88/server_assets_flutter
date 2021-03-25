4b2caec70c7c71c930b677aa3cd3e25fa8693d5b
Irving Didier <bider88@gmail.com>
2021-03-24 22:24:43 -0600

    inti to start downloader from remote server

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,119 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-void main() {
-  runApp(MyApp());
-}
+import 'package:archive/archive.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:path_provider/path_provider.dart';
+
+const api =
+    'https://firebasestorage.googleapis.com/v0/b/storage-serve.appspot.com/o/1609870595474-GIMBeta4ene.zip?alt=media&token=3799fa86-6a28-49e3-843d-79d548d0c5ab';
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  MyHomePage();
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  String _dir;
+  String nameZip;
+  String name;
+  bool filesNotDownloaded = true;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    nameZip = _getNameZip();
+    name = _getNameWithoutZip();
+    _downloadAssets();
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+        child: filesNotDownloaded ? CircularProgressIndicator() :
+        Text('Contenidos descargados...'),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+  Future<void> _downloadAssets() async {
+    print('_downloadAssets>>>>>>>>>>>>>');
+    if (_dir == null) {
+      _dir = (await getApplicationDocumentsDirectory()).path;
+    }
+
+    if (!await _hasToDownloadAssets(name, _dir)) {
+      filesNotDownloaded = false;
+      return;
+    }
+
+    var zippedFile = await _downloadFile(api, '$nameZip', _dir);
+
+    var bytes = zippedFile.readAsBytesSync();
+    var archive = ZipDecoder().decodeBytes(bytes);
+
+    for (var file in archive) {
+      var filename = '$_dir/${file.name}';
+      print('filename:::::::::::: $filename');
+      if (file.isFile) {
+        var outFile = File(filename);
+        outFile = await outFile.create(recursive: true);
+        await outFile.writeAsBytes(file.content);
+      }
+    }
+  }
+
+  Future<bool> _hasToDownloadAssets(String name, String dir) async {
+    var file = File('$dir/$name.zip');
+    return !(await file.exists());
+  }
+
+  Future<File> _downloadFile(String url, String filename, String dir) async {
+    var req = await http.Client().get(Uri.parse(url));
+    var file = File('$dir/$filename');
+    return file.writeAsBytes(req.bodyBytes);
+  }
+
+  String _getNameZip() {
+    String keyToFind = '.zip';
+    int posZip = api.lastIndexOf(keyToFind);
+    String url = api.substring(0, posZip + keyToFind.length);
+    List<String> splitUrl = url.split('/');
+    String nameZip = splitUrl.lastWhere((split) => split.endsWith('.zip'));
+    return nameZip;
+  }
+  
+  String _getNameWithoutZip() {
+    List<String> split = nameZip.split('.zip');
+    return split[0];
+  }
+
+  Widget _getImage(String name, String dir) {
+    // if (_theme != AppTheme.candy) {
+    //   var file = _getLocalImageFile(name, dir);
+    //   return Image.file(file);
+    // }
+    return Image.asset('assets/images/$name');
+  }
+
+  File _getLocalImageFile(String name, String dir) => File('$dir/$name');
 }
